662457dfb74587287ac455d5d1659c7e7c89c4db
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-01-31 15:18:13 +0100

    Fix broken test.

--- tests/phpunit/tests/load.php
+++ tests/phpunit/tests/load.php
@@ -17,7 +17,8 @@ class Tests_Load extends Unit_Test_Case {
 	 * @expectedIncorrectUsage awsmug\Torro_Forms\Components\Extensions::register()
 	 */
 	public function test_torro_forms_plugin_boilerplate_load() {
-		$result = torro_forms_plugin_boilerplate_load();
+		// This must error because the extension will already be registered.
+		$result = torro_forms_plugin_boilerplate_load( torro() );
 
 		$this->assertWPError( $result );
 	}
